91313a8f236ddd10d48f0dc0942095905e1bbcfe
Bùi Thành Senpai <bthanh2701@gmail.com>
2022-07-27 00:26:00 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const path = require('path')
 const PORT = process.env.PORT || 3000;
 const route = require("./route")
 const errorHandler = require("../middlewares/errorHandler");
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors({
+  origin : '*'
+}))
 app.use("/api/v2",route)
 app.use(errorHandler);
 
